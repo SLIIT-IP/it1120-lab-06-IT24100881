ed6d25c61a14537a19375cc630436ab313a5d63f
IT24100881 <it24100881@my.sliit.lk>
2024-08-27 23:39:21 -0700

    Add IT24100881Lab6Q1.java to Lab6Q1

---
+++ Q1/IT24100881Lab6Q1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class IT24100881Lab6Q1{
+
+	public static void main(String[] args){
+
+Scanner input=new Scanner(System.in);
+
+	System.out.println("Enter a number: ");
+	int number=input.nextInt();
+
+int square;
+
+square=number*number;
+
+	System.out.println("The square is: "+square);
+
+double square_root;
+
+square_root=Math.sqrt(number);
+
+	System.out.println("Square root is: "+square_root);
+
+	}
+}
