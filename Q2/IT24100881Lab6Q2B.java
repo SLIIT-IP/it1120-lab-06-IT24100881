64432d5a2926c2e1a11ee4f10c77eb8491c5b44a
IT24100881 <it24100881@my.sliit.lk>
2024-08-28 02:18:58 -0700

    Add  IT24100881Lab6Q2B.java to Lab6Q2B

---
+++ Q2/IT24100881Lab6Q2B.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IT24100881Lab6Q2B{
+
+public static void main(String[] args){
+
+Scanner input=new Scanner(System.in);
+
+int i=1;
+
+while(10>=i){
+
+	System.out.println("Enter number"+i+":");
+	int number=input.nextInt();
+
+System.out.println("You entered "+number);
+
+i++;
+}
+	}
+}
